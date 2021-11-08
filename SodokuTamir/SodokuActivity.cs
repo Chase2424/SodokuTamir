99c884289a390f6716124b763a56906e3738066f
StudentC <StudentC@CYBERN-03>
2021-11-08 11:23:50 +0200

    8.11.2021 part 2

--- SodokuTamir/SodokuActivity.cs
+++ SodokuTamir/SodokuActivity.cs
@@ -14,18 +14,31 @@ namespace SodokuTamir
     [Activity(Label = "Sodoku")]
     public class SodokuActivity : Activity
     {
+        SudokuCell[][]table;
+        String input;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.SodokuLayout);
             // Create your application here
+            this.table = new SudokuCell[9][9];
             SudokuCell[,] cells = new SudokuCell[9, 9];
-
+            input = "2 9 ? 7 4 3 8 6 1"+
+                    "4 7 1 8 6 5 9 ? 7"+
+                    "8 7 6 1 9 2 5 4 3"+
+                    "3 8 7 4 5 9 2 1 6"+
+                    "6 1 2 3 ? 7 4 7 5"+
+                    "? 4 9 2 ? 6 7 3 8"+
+                    "? ? 3 5 2 4 1 8 9"+
+                    "9 2 8 6 7 1 ? 5 4"+
+                    "1 5 4 9 3 ? 6 7 2";
+            String [] split = input.split(regex:" ");
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
                 {
-                  cells[i, j] = new SudokuCell();
+                  String s =split[i*9+j];
+                  table[i, j] = new SudokuCell();
                   
                 }
             }
--- SodokuTamir/SudokuCell.cs
+++ SodokuTamir/SudokuCell.cs
@@ -14,40 +14,45 @@ namespace SodokuTamir
     class SudokuCell :Square
     {
         public int Value { get; set; }
-        public bool IsLocked { get; set; }
+        public bool Fixed { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
         Button btn;
         public void Clear()
         {
             this.Value = "";
-            this.IsLocked = false;
+            this.Fixed= false;
         }
         public SudokuCell(int Value,int x,int y,Context context)
         {
             if(this.Value!=0)
             {
-
+                this.Fixed = true;
+            }
+            else
+            {
+                this.Fixed = false;
             }
             this.Value = Value;
+            this.btn = new Button(context);
             this.X = X;
             this.Y = Y;
-            if(this.IsLocked == false)
+            if(this.Fixed == false)
             {
                 btn.SetText(String.valueOf(this.Value));
             }
-            this.btn.Click +=
+            this.btn.Click += Btn_Click;
                
 
 
     }
-        /*
+       
          private void Btn_Click(object sender, System.EventArgs e)
                 {
-                    if(!isLocked)
+                    if(Fixed)
                          {
                             
                          }
                 }
-        */
+       
 }
